ee97e3371ead2bc6db0444227509295b0beb725b
Elvis <okorieonyekachi003@gmail.com>
2025-11-13 02:40:39 +0100

    recent

--- NEW/settings.py
+++ NEW/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-n(^x+qbg%ff)&8rxql@s&7__^t7kl$2t4a8ac+o29ep-g1=k7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [# settings.py
+
+ALLOWED_HOSTS = ["aclerery.site", "www.aclerery.site"]
+]
 
 
 # Application definition
